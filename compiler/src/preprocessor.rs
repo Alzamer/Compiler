aa5675490cee92e316f94bcc1d3e5b5d8deab043
alzamer <mic.szendzielorz@gmail.com>
2023-10-16 23:18:11 +0200

    Preprocessor delete comments

--- compiler/src/preprocessor.rs
+++ compiler/src/preprocessor.rs
@@ -9,6 +9,11 @@ pub fn delete_single_line_comments(source: &mut String) -> () {
 	}
 }
 
-fn delete_multi_line_comments(source: String) -> () {
-	let re = Regex::new(r"").unwrap();	
+pub fn delete_multi_line_comments(source: &mut String) -> () {
+	let re = Regex::new(r"\/\*.*\*\/").unwrap();	
+	let matched: Vec<_> = re.find_iter(source).map(|m| (m.start(), m.end())).collect();
+	
+	for i in matched.iter().rev(){
+		source.drain(i.0..i.1);
+	}
 }
