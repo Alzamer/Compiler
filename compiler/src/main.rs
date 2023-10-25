8083638884f9b71f14a294525648a982659190d0
alzamer <mic.szendzielorz@gmail.com>
2023-10-25 16:42:37 +0200

    Added handling #define macro

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -6,17 +6,18 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 1{
+    if args.len() == 1 {
         println!("Program arguments:");
         println!("<FILENAME>");
         ()
     }
     
-    let mut file_contents = fs::read_to_string(&args[1])
+    let mut file_content = fs::read_to_string(&args[1])
         .expect("LogRocket: Should have been able to read the file");
 
-    preprocessor::delete_multi_line_comments(&mut file_contents);
-    preprocessor::delete_single_line_comments(&mut file_contents);
+    preprocessor::delete_multi_line_comments(&mut file_content);
+    preprocessor::delete_single_line_comments(&mut file_content);
+    preprocessor::handle_macros(&mut file_content);
 
-    println!("{file_contents}");
+    println!("{:?}", file_content);
 }
--- compiler/src/preprocessor.rs
+++ compiler/src/preprocessor.rs
@@ -1,10 +1,11 @@
 use regex::Regex;
+use std::collections::HashMap;
 
 pub fn delete_single_line_comments(source: &mut String) -> () {
 	let re = Regex::new(r"//.*\n").unwrap();	
 	let matched: Vec<_> = re.find_iter(source).map(|m| (m.start(), m.end())).collect();
 
-	for i in matched.iter().rev(){
+	for i in matched.iter().rev() {
 		source.drain(i.0..i.1);
 	}
 }
@@ -13,7 +14,33 @@ pub fn delete_multi_line_comments(source: &mut String) -> () {
 	let re = Regex::new(r"\/\*(.|\n|\r\n)*?\*\/").unwrap();	
 	let matched: Vec<_> = re.find_iter(source).map(|m| (m.start(), m.end())).collect();
 	
-	for i in matched.iter().rev(){
+	for i in matched.iter().rev() {
 		source.drain(i.0..i.1);
 	}
+}
+
+pub fn handle_macros(file_content: &mut String) -> String {
+	let re = Regex::new(r"#define [a-zA-Z]{1}[a-zA-Z0-9]* [a-zA-Z0-9]+$").unwrap();
+	let lines : Vec<_> = file_content.split("\r\n").map(|m| m.trim()).collect();
+	let mut macros = HashMap::new();
+	let mut result : Vec<_> = Vec::new();
+
+	for line in lines {
+		let mut altered_line = line.to_string();
+
+		if let Some(matched) = re.find(line) {
+			let matched_macro = &line[matched.start()..matched.end()].split(" ").collect::<Vec<_>>();
+			macros.insert(matched_macro[1], matched_macro[2]);
+			altered_line.replace_range(matched.start()..matched.end(), "");
+		}
+
+		for key in macros.keys() {
+			altered_line = altered_line.replace(key, macros.get(key).unwrap());
+		}
+
+		result.push(altered_line);
+	}
+
+	*file_content = result.join("");
+	file_content.to_string()
 }
